8fa7184b68fd751dc0dd20507bb4142cdf924ca3
Yuratama1 <yuratama661@gmail.com>
2025-05-27 18:21:52 +0700

    menambahkan prosedure tambah anak

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -17,4 +17,15 @@ class ibu{
         void tambahanak(anak*);
         void cetakAnak();
 };
+void ibu::tambahanak(anak* pAnak){
+    daftar_anak.push_back(pAnak);
+}
+void ibu::cetakAnak(){
+    cout << "Daftar Anak Dari Ibu \"" <<
+    this->nama << "\":\n";
+    for (int i = 0; i < daftar_anak.size(); i++){
+        cout << daftar_anak[i]->nama << endl;
+    }
+    cout << endl;
+}
 #endif
